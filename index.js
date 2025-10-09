2791a751a994403132fce8bd3113a66ff31170ed
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-10-09 15:22:54 +0200

    push

---
+++ index.js
@@ -0,0 +1,32 @@
+const Name='mel';
+console.log(Name);
+
+const age='15';
+console.log(age);
+
+isStudent=true;
+console.log(isStudent);
+
+const myString='I think therefore I am';
+console.log(myString);
+
+const myNumber='42';
+console.log(myNumber + 10);
+
+const myNull='0';
+console.log(myNull);
+
+
+
+alert("Warning! The action may be dangerous");
+const confirmAction= confirm("Are you sure you want to continue?");
+console.log(confirmAction);
+
+
+const userAnswer=confirm("Are you sure you wan to leave the page?");
+console.log(userAnswer)
+
+
+const userName= prompt("Enter your name");
+alert(userName);
+console.log(typeof userName);
