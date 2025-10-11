813e2f7f335bc62369a663ef3b85be4d5764bb90
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-10-11 17:17:05 +0200

    push

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-const Name='mel';
-console.log(Name);
+let celsius=30;
+let fahreinheit = (celsius / 3)+30;
+console.log(fahreinheit);
 
-const age='15';
-console.log(age);
+let daysInJanuary = 30;
+let hoursInJanuary = (daysInJanuary*24);
+let minutesInJanuary = (hoursInJanuary*60);
+console.log(minutesInJanuary)
 
-isStudent=true;
-console.log(isStudent);
+let health = 100
+let energy = 38
+console.log("Health status:" health - 2)
 
-const myString='I think therefore I am';
-console.log(myString);
+console.log("Energy left:"energy - 10)
 
-const myNumber='42';
-console.log(myNumber + 10);
-
-const myNull='0';
-console.log(myNull);
-
-
-
-alert("Warning! The action may be dangerous");
-const confirmAction= confirm("Are you sure you want to continue?");
-console.log(confirmAction);
+let totalPrice = 55$
+console.log(totalPrice * 0.1"$")
 
 
-const userAnswer=confirm("Are you sure you wan to leave the page?");
-console.log(userAnswer)
+const floatNumber = 12.78
+let roundedDown = Math.floor(floatNumber) 
+console.log(roundedDown)
+
+const floatString = "45.67"
+let parsedFloat = parseFloat(floatString)
+console.log(parsedFloat)
+
+const intString = "123"
+let parsedIntта = parseInt(intString) 
+console.log(parsedIntта)
 
 
-const userName= prompt("Enter your name");
-alert(userName);
-console.log(typeof userName);
+const number = 5783
+const sqrtNumber = Math.sqrt(number)
+console.log(sqrtNumber)
+
+const integer = 42
+const stringNumber = "256"
+const convertedInt = parseInt(stringNumber)
+console.log(convertedInt)
+
+const convertedString = toString(integer)
+console.log(convertedString)
